08cb32ed29b8dcff616208c830fa8a10b061c684
iamsamhhh <sc_samchen@icloud.com>
2024-10-30 18:08:03 +0800

    10-skin-system: Created skin class

---
+++ Assets/Scripts/Skin.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new skin", menuName = "Skin/Create skin")]
+public class Skin : ScriptableObject
+{
+    public bool haveTrail;
+    public Color trailColor;
+    public Sprite sprite;
+    public Color spriteColor;
+
+}
