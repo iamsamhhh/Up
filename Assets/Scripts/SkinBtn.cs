1cab191612772d0144ab18e8c9f36c6d77358f4d
iamsamhhh <sc_samchen@icloud.com>
2024-10-31 19:22:01 +0800

    10-skin-system: Added pop up to confirm purchase

--- Assets/Scripts/SkinBtn.cs
+++ Assets/Scripts/SkinBtn.cs
@@ -1,7 +1,8 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+using SFramework;
+using System;
 public class SkinBtn : MonoBehaviour
 {
     public Skin skin;
@@ -14,10 +15,20 @@ public class SkinBtn : MonoBehaviour
         }
         else{
             if (GameManager.instance.coinCount >= skin.cost){
-                GameManager.instance.coinCount -= skin.cost;
-                GameManager.instance.currentSkin = skin;
-                skin.bought = true;
+                GUIMgr.instance.AddPopUp(
+                    "Are you sure you want to buy this skin that cost " + skin.cost + " Coin?",
+                    OnConfirmBtn,
+                    onCancelBtn
+                    );
             }
         }
     }
+
+    private void OnConfirmBtn(){
+        GameManager.instance.coinCount -= skin.cost;
+        GameManager.instance.currentSkin = skin;
+        skin.bought = true;
+    }
+
+    private void onCancelBtn(){}
 }
